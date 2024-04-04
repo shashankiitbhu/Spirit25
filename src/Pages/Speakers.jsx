150ae81696f643db5d04e0191017a784144945d4
Shivam Prajapati <shivam.prajapati.phe21@itbhu.ac.in>
2024-04-04 19:54:16 +0530

    speakers

--- src/Pages/Speakers.jsx
+++ src/Pages/Speakers.jsx
@@ -32,64 +32,87 @@ const Speakers = () => {
       name: "Dr. Rajeev Raghuvanshi",
       image: "/speakers/rajeev.png",
       des: "Drugs Controller General OF INDIA",
-      logo:"/speakers/dcgi.png",
-      company:'CDSCO'
+      logo: "/speakers/dcgi.png",
+      company: "CDSCO",
     },
     {
       name: "Dr. Dulal Panda",
       image: "/speakers/panda.png",
       des: "Director",
-      logo:"/speakers/niper.jpg",
-      company:'NIPER Mohali'
+      logo: "/speakers/niper.jpg",
+      company: "NIPER Mohali",
     },
     {
       name: "Dr. Birendra Singh",
       image: "/speakers/birendra.png",
       des: "President, Global Quality Head",
-      logo:"/speakers/mankind.png",
-      company:'Mankind Pharma',
+      logo: "/speakers/mankind.png",
+      company: "Mankind Pharma",
     },
     {
       name: "Dr. Anil Kukreja",
       image: "/speakers/anil.png",
       des: "Vice President, Medical Affairs and Regulatory",
-      logo:"/speakers/astra.png",
-      company:'AstraZeneca'
+      logo: "/speakers/astra.png",
+      company: "AstraZeneca",
     },
     {
       name: "Dr. Amit Vaish",
       image: "/speakers/amit.png",
       des: "Principal Scientist",
-      logo:"/speakers/amgen.png",
-      company:'Amgen'
+      logo: "/speakers/amgen.png",
+      company: "Amgen",
     },
     {
       name: "Dr. Romi Singh",
       image: "/speakers/romi.png",
       des: "Senior Vice President",
-      logo:"/speakers/sun-pharma.png",
-      company:'Sun Pharma'
+      logo: "/speakers/sun-pharma.png",
+      company: "Sun Pharma",
+    },
+    {
+      name: "Dr. Navratna Vajpai",
+      image: "/speakers/Navratna-Vajpai.png",
+      des: "Chief Scientific Manager",
+      logo: "/speakers/janssen.png",
+      company: "Biocon Biologics",
     },
     {
       name: "Dr. Anish Chakkumkal",
       image: "/speakers/anish.png",
       des: "Associate Scientific Director",
-      logo:"/speakers/janssen.png",
-      company:'Johnson & Johnson'
+      logo: "/speakers/janssen.png",
+      company: "Johnson & Johnson",
     },
     {
-      name: "Avinash Narwaria",
-      image: "/speakers/avinash.jpg",
-      des: " Vice President",
-      logo:"/speakers/janssen.png",
-      company:'Emami'
+      name: "Dr. Rajeev Pandey",
+      image: "/speakers/rajeevPandey.png",
+      des: "Laboratory Director",
+      logo: "/speakers/janssen.png",
+      company: "Strand",
+    },
+    {
+      name: "Dr. Ankit Saneja",
+      image: "/speakers/ankit.png",
+      des: "Scientist",
+      logo: "/speakers/janssen.png",
+      company: "CSIR-IHBT India",
+    },
+    {
+      name: "Dr. Prakash Muthudoss",
+      image: "/speakers/prakash.png",
+      des: "Chief Executive Officer",
+      logo: "/speakers/janssen.png",
+      company: "A2Z4.0 ",
     },
   ];
   return (
-    <div className={styles.page} id="speakers">
+    <div
+      className={styles.page}
+      id="speakers">
       <h1>Keynote Speakers</h1>
       <div className={styles.list}>
-        {speakers.slice(0, 8).map((item, index) => {
+        {speakers.map((item, index) => {
           return (
             <Card
               item={item}
